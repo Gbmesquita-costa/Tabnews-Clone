ef1542cef5b8f5aff7a0f6aec542e387ef6e02f8
Gbmesquita-costa <mesquitag313@gmail.com>
2024-06-17 22:43:55 -0300

    add `database.js` and `.env` files

---
+++ src/app/api/v1/status/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+import database from "@/../infra/database";
+
+export const GET = async (req: NextRequest) => {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+
+  return NextResponse.json(
+    { message: "Alunos do curso.dev são sensacionais" },
+    {
+      status: 200,
+    },
+  );
+};
