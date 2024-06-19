011e520c7398945c907a612734d9848136c1851f
Gbmesquita-costa <mesquitag313@gmail.com>
2024-06-19 17:14:29 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,8 +1,28 @@
+interface ApiStatusProps {
+  updated_at: Date;
+  dependencies: {
+    database: {
+      version: string;
+      max_connections: number;
+      opened_connections: number;
+    };
+  };
+}
+
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const statusCode = response.status;
 
   expect(statusCode).toBe(200);
 
-  console.log("Passou na bateria de testes");
+  const { updated_at, dependencies } =
+    (await response.json()) as ApiStatusProps;
+
+  const parsedUpdatedAt = new Date(updated_at).toISOString();
+  expect(updated_at).toEqual(parsedUpdatedAt);
+
+  expect(dependencies.database.version).toEqual("16.0");
+  expect(dependencies.database.max_connections).toEqual(100);
+
+  expect(dependencies.database.opened_connections).toEqual(1);
 });
